396a8d4d9fd86aaa35178ee87c14ffa70a0a290b
ZgsNat <nguyenthaithinh9876@gmail.com>
2026-01-23 23:41:30 +0700

    update: clean frontend

--- frontend/src/api/axiosClient.js
+++ frontend/src/api/axiosClient.js
@@ -4,14 +4,12 @@ const axiosClient = axios.create({
   baseURL: 'http://localhost:8000/api/v1',
   timeout: 10000,
   headers: {
-    'Content-Type': 'application/json',  // We send JSON
-    'Accept': 'application/xml'          // We expect XML back
+    'Content-Type': 'application/json',
+    'Accept': 'application/xml'
   },
-  // Important: We need the raw text to parse XML ourselves
   responseType: 'text', 
 });
 
-// Interceptor to handle Blob responses (for Excel download) separately
 axiosClient.interceptors.response.use((response) => {
   return response;
 }, (error) => {
